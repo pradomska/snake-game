7106f71a3787c999a7fac81335c74f05eceb1f52
Paulina Radomska <paulinaradomska10@wp.pl>
2023-04-12 14:54:16 +0200

    adding files

--- scoreboard.py
+++ scoreboard.py
@@ -8,7 +8,12 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+<<<<<<< HEAD
         self.high_score = 0
+=======
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
+>>>>>>> 74bd248 (adding files)
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -26,6 +31,11 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+<<<<<<< HEAD
+=======
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
+>>>>>>> 74bd248 (adding files)
         self.score = 0
         self.update_scoreboard()
 
